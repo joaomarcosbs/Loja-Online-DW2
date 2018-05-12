79b4d81c0d8208c52ce8df93704534fafffceca1
joaomarcosbs <joaomarcosbs@outlook.com>
2018-05-12 00:21:58 -0300

    Alterações de layout
    Adicionada página de cadastro do usuário e login (inacabada), alterada nav-bar e footer.

---
+++ login.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	   
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Login</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+</head>
+<body>
+
+	<!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+
+      <div class="container">
+        <a class="navbar-brand" href="#">IF - Tech Shop</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Sobre</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Perfil</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Carrinho</a>
+            </li>
+          </ul>
+        </div>
+        <form class="form-inline" style="margin-left: 30px">
+          <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+        </form>
+      </div>
+    </nav>
+
+
+    <div class="container">
+    	
+    	<form class="form-signin" style="text-align: center;">
+      		
+      		<h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      		<label for="inputEmail" class="sr-only">Email address</label>
+      		<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      		<label for="inputPassword" class="sr-only">Password</label>
+      		<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      		<div class="checkbox mb-3">
+        		<label>
+          			<input type="checkbox" value="remember-me"> Remember me
+        		</label>
+      		</div>
+      		<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      		<p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+   		</form>
+
+    </div>
+
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; IF - Tech Shop  2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
